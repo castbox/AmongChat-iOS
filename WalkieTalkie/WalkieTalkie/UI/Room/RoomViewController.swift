b5b9885e86b31cccfb8b6afad87cfe454801e7ba
袁仕崇 <xiaochong2154@163.com>
2020-05-11 15:04:30 +0800

    IMPROVE: // logic to change the role

--- WalkieTalkie/WalkieTalkie/UI/Room/RoomViewController.swift
+++ WalkieTalkie/WalkieTalkie/UI/Room/RoomViewController.swift
@@ -556,7 +556,7 @@ private extension RoomViewController {
             }
             .filter { [weak self] value in
                 guard let `self` = self else { //talking 状态不能改变
-                        return false
+                    return false
                 }
                 //connecting -> spaak
                 if value { //prepareing to talking
@@ -576,8 +576,16 @@ private extension RoomViewController {
                 return true
             }
             .distinctUntilChanged()
+            .debug()
+            .flatMap { value -> Observable<Bool> in
+                guard !value else {
+                    return Observable.just(value)
+                }
+                return Observable.just(value)
+                    .delay(.fromSeconds(0.3), scheduler: MainScheduler.asyncInstance)
+            }
+            .debug()
             .subscribe(onNext: { [weak self] highlighted in
-//                cdPrint("speakButton is highlighted: \(highlighted)")
                 self?.speakButton.setImage(highlighted ? R.image.speak_button_pre() : R.image.speak_button_nor(), for: .normal)
                 self?.speakButton.alpha = highlighted ? 1 : 0.6
                 self?.state = highlighted ? .preparing : .connected
