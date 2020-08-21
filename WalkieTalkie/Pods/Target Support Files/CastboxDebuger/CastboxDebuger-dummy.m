62754817d9892a884b13f089f6405eceb01ddbd9
mayue <yue.ma@castbox.fm>
2020-08-21 16:02:19 +0800

    mod: track pods dir

---
+++ WalkieTalkie/Pods/Target Support Files/CastboxDebuger/CastboxDebuger-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CastboxDebuger : NSObject
+@end
+@implementation PodsDummy_CastboxDebuger
+@end
---
+++ WalkieTalkie/Pods/TikTokOpenSDK/TikTokOpenSDK.framework/Headers/TikTokOpenSDKShare.h
@@ -0,0 +1,19 @@
+//
+//  TikTokOpenSDKShare.h
+//
+//  Created by TikTok on 2020/2/3.
+//  Copyright (c) 2020年 TikTok. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <BytedanceOpenPlatformSDK/BDOpenPlatformShare.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TikTokOpenSDKShareRequest : BDOpenPlatformShareRequest
+
+- (instancetype)init;
+
+@end
+
+NS_ASSUME_NONNULL_END
