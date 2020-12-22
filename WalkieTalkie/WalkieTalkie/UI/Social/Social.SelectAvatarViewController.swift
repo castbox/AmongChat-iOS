86d5725c5f7a8702d5505747b103fe887206c78a
mayue <yue.ma@castbox.fm>
2020-12-22 22:54:42 +0800

    mod: fixed avatar size

--- WalkieTalkie/WalkieTalkie/UI/Social/Social.SelectAvatarViewController.swift
+++ WalkieTalkie/WalkieTalkie/UI/Social/Social.SelectAvatarViewController.swift
@@ -350,9 +350,9 @@ extension Social.SelectAvatarViewController {
         
         private lazy var avatarIV: UIImageView = {
             let iv = UIImageView()
+            iv.layer.cornerRadius = 45
             iv.layer.masksToBounds = true
             iv.contentMode = .scaleToFill
-            iv.isHidden = true
             return iv
         }()
         
@@ -370,12 +370,6 @@ extension Social.SelectAvatarViewController {
             iv.isHidden = true
             return iv
         }()
-                        
-        override func layoutSubviews() {
-            super.layoutSubviews()
-            avatarIV.isHidden = false
-            avatarIV.layer.cornerRadius = avatarIV.bounds.width / 2
-        }
         
         override init(frame: CGRect) {
             super.init(frame: .zero)
@@ -393,9 +387,8 @@ extension Social.SelectAvatarViewController {
             contentView.addSubviews(views: avatarIV, selectedIcon, adBadge)
             
             avatarIV.snp.makeConstraints { (maker) in
-                maker.left.right.equalToSuperview().inset(34)
-                maker.width.equalTo(avatarIV.snp.height).multipliedBy(1)
-                maker.centerY.equalToSuperview()
+                maker.width.height.equalTo(90)
+                maker.center.equalToSuperview()
             }
             
             selectedIcon.snp.makeConstraints { (maker) in
