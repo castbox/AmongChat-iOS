397f6097a78aeb716489010ed948c32d34ccb5eb
mayue <yue.ma@castbox.fm>
2021-02-19 17:38:31 +0800

    mod: fix new added friend order

--- WalkieTalkie/WalkieTalkie/UI/AmongUs/Home/AmongChat.Home+RelationViewModels.swift
+++ WalkieTalkie/WalkieTalkie/UI/AmongUs/Home/AmongChat.Home+RelationViewModels.swift
@@ -169,7 +169,7 @@ extension AmongChat.Home {
             }
             
             var onlineFriends = playingsRelay.value
-            onlineFriends.append(user)
+            onlineFriends.insert(user, at: 0)
             
             suggestStrangerRelay.accept(suggestionUsers)
             playingsRelay.accept(onlineFriends)
