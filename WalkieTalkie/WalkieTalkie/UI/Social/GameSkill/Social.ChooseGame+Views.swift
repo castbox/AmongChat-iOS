b51ade24209a90c24893c1d4cb384bebc9f8e840
mayue <yue.ma@castbox.fm>
2021-03-25 20:20:02 +0800

    mod: fix gradient view

--- WalkieTalkie/WalkieTalkie/UI/Social/GameSkill/Social.ChooseGame+Views.swift
+++ WalkieTalkie/WalkieTalkie/UI/Social/GameSkill/Social.ChooseGame+Views.swift
@@ -81,7 +81,7 @@ extension Social.ChooseGame {
         let l = v.layer
         l.colors = [UIColor(hex6: 0x121212, alpha: 0).cgColor, UIColor(hex6: 0x121212, alpha: 0.18).cgColor, UIColor(hex6: 0x121212, alpha: 0.57).cgColor, UIColor(hex6: 0x121212).cgColor]
         l.startPoint = CGPoint(x: 0.5, y: 0)
-        l.endPoint = CGPoint(x: 0.5, y: 0.41)
+        l.endPoint = CGPoint(x: 0.5, y: 0.6)
         l.locations = [0, 0.3, 0.6, 1]
         return v
     }
