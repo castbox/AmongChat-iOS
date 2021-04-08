d7ffdd48e93593249295c54375d58521b78741b7
mayue <yue.ma@castbox.fm>
2021-04-08 12:35:38 +0800

    mod: refractor of group info view

--- WalkieTalkie/WalkieTalkie/UI/Group/FansGroup.GroupInfoViewController.swift
+++ WalkieTalkie/WalkieTalkie/UI/Group/FansGroup.GroupInfoViewController.swift
@@ -46,6 +46,7 @@ extension FansGroup {
         
         private lazy var groupHeaderView: GroupHeaderView = {
             let h = GroupHeaderView()
+            h.frame = CGRect(origin: .zero, size: CGSize(width: Frame.Screen.width, height: 254))
             h.leaveHandler = { [weak self] in
                 self?.leaveGroup()
             }
@@ -59,25 +60,7 @@ extension FansGroup {
             return h
         }()
         
-        private lazy var tableView: UITableView = {
-            let tb = UITableView(frame: .zero, style: .grouped)
-            tb.register(MemberCell.self, forCellReuseIdentifier: NSStringFromClass(MemberCell.self))
-            tb.register(AddMemberCell.self, forCellReuseIdentifier: NSStringFromClass(AddMemberCell.self))
-            tb.dataSource = self
-            tb.delegate = self
-            tb.rowHeight = 69
-            tb.separatorStyle = .none
-            tb.backgroundColor = .clear
-            groupHeaderView.frame = CGRect(origin: .zero, size: CGSize(width: Frame.Screen.width, height: 254))
-            tb.tableHeaderView = groupHeaderView
-            if #available(iOS 11.0, *) {
-                tb.contentInsetAdjustmentBehavior = .never
-            } else {
-                // Fallback on earlier versions
-                automaticallyAdjustsScrollViewInsets = false
-            }
-            return tb
-        }()
+        private var tableView: UITableView!
         
         private lazy var applyButton: UIButton = {
             let btn = UIButton(type: .custom)
@@ -108,10 +91,6 @@ extension FansGroup {
             return v
         }()
         
-        private let membersRelay = BehaviorRelay<[Entity.UserProfile]>(value: [])
-        private var hasMoreData = true
-        private var isLoading = false
-        
         private let groupId: String
         private var groupInfoViewModel: GroupViewModel? = nil {
             didSet {
@@ -134,176 +113,8 @@ extension FansGroup {
         override func viewDidLoad() {
             super.viewDidLoad()
             setUpLayout()
-            setUpEvents()
             fetchInfo()
-            fetchMembers()
         }
-        
-    }
-    
-}
-
-extension FansGroup.GroupInfoViewController: UITableViewDataSource {
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        
-        guard let group = groupInfoViewModel else {
-            return 0
-        }
-        
-        if group.groupInfo.group.uid.isSelfUid {
-            return 3
-        } else {
-            return 2
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        guard let group = groupInfoViewModel else {
-            return 0
-        }
-        
-        switch (section, group.groupInfo.group.uid.isSelfUid) {
-        
-        case(0, _):
-            return 1
-        
-        case(1, true):
-            return 1
-            
-        default:
-            return membersRelay.value.count
-        }
-        
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        guard let group = groupInfoViewModel else {
-            return tableView.dequeueReusableCell(withIdentifier: NSStringFromClass(MemberCell.self), for: indexPath)
-        }
-        
-        switch (indexPath.section, group.groupInfo.group.uid.isSelfUid) {
-        
-        case (1, true):
-            let cell = tableView.dequeueReusableCell(withIdentifier: NSStringFromClass(AddMemberCell.self), for: indexPath)
-
-            guard let addCell = cell as? AddMemberCell else {
-                return cell
-            }
-            
-            addCell.tapHandler = { [weak self] in
-                guard let `self` = self else {
-                    return
-                }
-                let vc = FansGroup.AddMemberController(groupId: self.groupId)
-                self.navigationController?.pushViewController(vc, animated: true)
-            }
-            
-            return cell
-            
-        case (let section, _):
-            
-            let cell = tableView.dequeueReusableCell(withIdentifier: NSStringFromClass(MemberCell.self), for: indexPath)
-            
-            guard let memberCell = cell as? MemberCell else {
-                return cell
-            }
-            
-            if section == 0 {
-                memberCell.bind(user: group.groupInfo.group.broadcaster) {
-                    
-                }
-                
-            } else if let member = membersRelay.value.safe(indexPath.row) {
-                memberCell.bind(user: member) {
-                    
-                }
-            }
-            
-            return cell
-        }
-        
-    }
-    
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        
-        if section == 0 {
-            return 52
-        } else if section == 1{
-            return 26
-        } else {
-            return .leastNormalMagnitude
-        }
-        
-    }
-    
-    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        if section == 0 {
-            return 24
-        } else {
-            return .leastNormalMagnitude
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        
-        guard let group = groupInfoViewModel,
-              section != 2 else {
-            return nil
-        }
-        
-        
-        let v = UIView()
-        
-        if section == 0 {
-            
-            let l = UILabel()
-            l.font = R.font.nunitoExtraBold(size: 16)
-            l.textColor = UIColor(hex6: 0x898989)
-            l.text = R.string.localizable.amongChatGroupAdmin()
-            
-            v.addSubview(l)
-            l.snp.makeConstraints { (maker) in
-                maker.leading.trailing.equalToSuperview().inset(20)
-                maker.top.equalTo(24)
-                maker.height.equalTo(22)
-            }
-            
-        } else if section == 1 {
-            
-            let l = UILabel()
-            l.font = R.font.nunitoExtraBold(size: 16)
-            l.textColor = UIColor(hex6: 0x898989)
-            l.text = "\(group.groupInfo.group.membersCount - 1)" + " " + R.string.localizable.amongChatGroupMembers()
-            
-            v.addSubview(l)
-            l.snp.makeConstraints { (maker) in
-                maker.leading.trailing.equalToSuperview().inset(20)
-                maker.centerY.equalToSuperview()
-                maker.height.equalTo(22)
-            }
-        }
-        
-        return v
-        
-    }
-}
-
-extension FansGroup.GroupInfoViewController: UITableViewDelegate {
-    
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        
-        let distance = scrollView.contentOffset.y
-        
-        groupHeaderView.enlargeTopGbHeight(extraHeight: -distance)
-        
-        navView.snp.updateConstraints { (maker) in
-            maker.top.equalTo(topLayoutGuide.snp.bottom).offset(min(0, -distance / 3))
-        }
-        
-        navView.alpha = 1 - distance / 49
         
     }
     
@@ -313,11 +124,7 @@ extension FansGroup.GroupInfoViewController {
     
     private func setUpLayout() {
         
-        view.addSubviews(views: tableView, navView, bottomGradientView)
-        
-        tableView.snp.makeConstraints { (maker) in
-            maker.edges.equalToSuperview()
-        }
+        view.addSubviews(views: groupHeaderView, navView, bottomGradientView)
         
         navView.snp.makeConstraints { (maker) in
             maker.leading.trailing.equalToSuperview()
@@ -331,20 +138,8 @@ extension FansGroup.GroupInfoViewController {
             maker.height.equalTo(134)
         }
         
-        tableView.pullToLoadMore { [weak self] in
-            self?.fetchMembers()
-        }
     }
-    
-    private func setUpEvents() {
-        membersRelay
-            .subscribe(onNext: { [weak self] (_) in
-                self?.tableView.reloadData()
-            })
-            .disposed(by: bag)
         
-    }
-    
     private func fetchInfo() {
         
         let hudRemoval = self.view.raft.show(.loading)
@@ -352,43 +147,10 @@ extension FansGroup.GroupInfoViewController {
             .do(onDispose: {
                 hudRemoval()
             })
-            .subscribe(onSuccess: { [weak self] (info) in
-                guard let `self` = self else { return }
-                let g = GroupViewModel(groupInfo: info)
-                self.groupInfoViewModel = g
-                self.groupHeaderView.bindViewModel(g)
-                self.tableView.tableHeaderView?.frame.size = self.groupHeaderView.viewSize
-                self.tableView.reloadData()
+            .subscribe(onSuccess: { (info) in
+                self.updateContent(info)
             })
             .disposed(by: bag)
-    }
-    
-    private func fetchMembers() {
-        guard hasMoreData,
-              !isLoading else {
-            return
-        }
-        
-        isLoading = true
-        
-        Request.membersOfGroup(groupId,
-                               skipMs: membersRelay.value.last?.opTime ?? 0)
-            .do(onDispose: { [weak self] () in
-                self?.isLoading = false
-            })
-            .subscribe(onSuccess: { [weak self] (memberList) in
-                
-                guard let `self` = self else {
-                    return
-                }
-                var members = self.membersRelay.value
-                members.append(contentsOf: memberList.list)
-                self.membersRelay.accept(members)
-                self.hasMoreData = memberList.more
-                self.tableView.endLoadMore(memberList.more)
-            })
-            .disposed(by: bag)
-        
     }
     
     private func leaveGroup() {
@@ -417,4 +179,46 @@ extension FansGroup.GroupInfoViewController {
             .disposed(by: bag)
     }
     
+    private func updateContent(_ groupInfo: Entity.GroupInfo) {
+        
+        let g = GroupViewModel(groupInfo: groupInfo)
+        groupInfoViewModel = g
+        
+        let listVC = FansGroup.GroupMemberListViewController(with: groupInfo)
+        
+        addChild(listVC)
+        view.addSubview(listVC.view)
+        listVC.view.snp.makeConstraints { (maker) in
+            maker.edges.equalToSuperview()
+        }
+        view.sendSubviewToBack(listVC.view)
+        listVC.didMove(toParent: self)
+        
+        tableView = listVC.tableView
+        
+        groupHeaderView.bindViewModel(g)
+        groupHeaderView.frame.size = groupHeaderView.viewSize
+        
+        tableView.tableHeaderView = groupHeaderView
+        tableView.reloadData()
+        
+        tableView.rx.contentOffset
+            .subscribe(onNext: { [weak self] (point) in
+                
+                guard let `self` = self else { return }
+                
+                let distance = point.y
+                
+                self.groupHeaderView.enlargeTopGbHeight(extraHeight: -distance)
+                
+                self.navView.snp.updateConstraints { (maker) in
+                    maker.top.equalTo(self.topLayoutGuide.snp.bottom).offset(min(0, -distance / 3))
+                }
+                
+                self.navView.alpha = 1 - distance / 49
+                
+            })
+            .disposed(by: bag)
+
+    }
 }
---
+++ WalkieTalkie/WalkieTalkie/UI/Group/FansGroup.GroupMemberListViewController.swift
@@ -0,0 +1,255 @@
+//
+//  FansGroup.GroupMemberListViewController.swift
+//  WalkieTalkie
+//
+//  Created by mayue on 2021/4/8.
+//  Copyright © 2021 Guru Rain. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+extension FansGroup {
+    
+    class GroupMemberListViewController: WalkieTalkie.ViewController {
+        
+        private(set) lazy var tableView: UITableView = {
+            let tb = UITableView(frame: .zero, style: .grouped)
+            tb.register(MemberCell.self, forCellReuseIdentifier: NSStringFromClass(MemberCell.self))
+            tb.register(AddMemberCell.self, forCellReuseIdentifier: NSStringFromClass(AddMemberCell.self))
+            tb.dataSource = self
+            tb.delegate = self
+            tb.rowHeight = 69
+            tb.separatorStyle = .none
+            tb.backgroundColor = .clear
+            if #available(iOS 11.0, *) {
+                tb.contentInsetAdjustmentBehavior = .never
+            } else {
+                // Fallback on earlier versions
+                automaticallyAdjustsScrollViewInsets = false
+            }
+            return tb
+        }()
+        
+        private let membersRelay = BehaviorRelay<[Entity.UserProfile]>(value: [])
+        private var hasMoreData = true
+        private var isLoading = false
+        
+        private let groupInfo: Entity.GroupInfo
+        
+        init(with groupInfo: Entity.GroupInfo) {
+            self.groupInfo = groupInfo
+            super.init(nibName: nil, bundle: nil)
+        }
+        
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            setUpLayout()
+            setUpEvents()
+            fetchMembers()
+        }
+        
+    }
+    
+}
+
+extension FansGroup.GroupMemberListViewController: UITableViewDataSource {
+    
+    // MARK: - UITableViewDataSource
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        if groupInfo.group.uid.isSelfUid {
+            return 3
+        } else {
+            return 2
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        switch (section, groupInfo.group.uid.isSelfUid) {
+        
+        case(0, _):
+            return 1
+            
+        case(1, true):
+            return 1
+            
+        default:
+            return membersRelay.value.count
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        switch (indexPath.section, groupInfo.group.uid.isSelfUid) {
+        
+        case (1, true):
+            let cell = tableView.dequeueReusableCell(withIdentifier: NSStringFromClass(AddMemberCell.self), for: indexPath)
+            
+            guard let addCell = cell as? AddMemberCell else {
+                return cell
+            }
+            
+            addCell.tapHandler = { [weak self] in
+                guard let `self` = self else {
+                    return
+                }
+                let vc = FansGroup.AddMemberController(groupId: self.groupInfo.group.gid)
+                self.navigationController?.pushViewController(vc, animated: true)
+            }
+            
+            return cell
+            
+        case (let section, _):
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: NSStringFromClass(MemberCell.self), for: indexPath)
+            
+            guard let memberCell = cell as? MemberCell else {
+                return cell
+            }
+            
+            if section == 0 {
+                memberCell.bind(user: groupInfo.group.broadcaster) {
+                    
+                }
+                
+            } else if let member = membersRelay.value.safe(indexPath.row) {
+                memberCell.bind(user: member) {
+                    
+                }
+            }
+            
+            return cell
+        }
+        
+    }
+    
+}
+
+extension FansGroup.GroupMemberListViewController: UITableViewDelegate {
+    
+    // MARK: - UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        
+        if section == 0 {
+            return 52
+        } else if section == 1{
+            return 26
+        } else {
+            return .leastNormalMagnitude
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        if section == 0 {
+            return 24
+        } else {
+            return .leastNormalMagnitude
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        guard section != 2 else {
+            return nil
+        }
+        
+        let v = UIView()
+        
+        if section == 0 {
+            
+            let l = UILabel()
+            l.font = R.font.nunitoExtraBold(size: 16)
+            l.textColor = UIColor(hex6: 0x898989)
+            l.text = R.string.localizable.amongChatGroupAdmin()
+            
+            v.addSubview(l)
+            l.snp.makeConstraints { (maker) in
+                maker.leading.trailing.equalToSuperview().inset(20)
+                maker.top.equalTo(24)
+                maker.height.equalTo(22)
+            }
+            
+        } else if section == 1 {
+            
+            let l = UILabel()
+            l.font = R.font.nunitoExtraBold(size: 16)
+            l.textColor = UIColor(hex6: 0x898989)
+            l.text = "\(groupInfo.group.membersCount - 1)" + " " + R.string.localizable.amongChatGroupMembers()
+            
+            v.addSubview(l)
+            l.snp.makeConstraints { (maker) in
+                maker.leading.trailing.equalToSuperview().inset(20)
+                maker.centerY.equalToSuperview()
+                maker.height.equalTo(22)
+            }
+        }
+        
+        return v
+        
+    }
+    
+}
+
+extension FansGroup.GroupMemberListViewController {
+    
+    private func setUpLayout() {
+        
+        view.addSubviews(views: tableView)
+        
+        tableView.snp.makeConstraints { (maker) in
+            maker.edges.equalToSuperview()
+        }
+        
+        tableView.pullToLoadMore { [weak self] in
+            self?.fetchMembers()
+        }
+    }
+    
+    private func setUpEvents() {
+        membersRelay
+            .subscribe(onNext: { [weak self] (_) in
+                self?.tableView.reloadData()
+            })
+            .disposed(by: bag)
+    }
+    
+    private func fetchMembers() {
+        guard hasMoreData,
+              !isLoading else {
+            return
+        }
+        
+        isLoading = true
+        
+        Request.membersOfGroup(groupInfo.group.gid,
+                               skipMs: membersRelay.value.last?.opTime ?? 0)
+            .do(onDispose: { [weak self] () in
+                self?.isLoading = false
+            })
+            .subscribe(onSuccess: { [weak self] (memberList) in
+                
+                guard let `self` = self else {
+                    return
+                }
+                var members = self.membersRelay.value
+                members.append(contentsOf: memberList.list)
+                self.membersRelay.accept(members)
+                self.hasMoreData = memberList.more
+                self.tableView.endLoadMore(memberList.more)
+            })
+            .disposed(by: bag)
+        
+    }
+    
+}
