8c57fd2e3038705c5d5b09ff7132ef6057b32667
袁仕崇 <xiaochong2154@163.com>
2021-05-06 19:21:28 +0800

    FIX: // retain cycle

--- WalkieTalkie/WalkieTalkie/UI/Group/FansGroup.GroupInfoViewController.swift
+++ WalkieTalkie/WalkieTalkie/UI/Group/FansGroup.GroupInfoViewController.swift
@@ -179,8 +179,8 @@ extension FansGroup.GroupInfoViewController {
             .do(onDispose: {
                 hudRemoval()
             })
-            .subscribe(onSuccess: { (info) in
-                self.updateContent(info)
+            .subscribe(onSuccess: { [weak self] (info) in
+                self?.updateContent(info)
             })
             .disposed(by: bag)
     }
