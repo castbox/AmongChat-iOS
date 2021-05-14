e4bcb4d86f1b2319b3e21755448a70c2552d6f5a
mayue <yue.ma@castbox.fm>
2021-05-14 17:23:11 +0800

    mod: fix live tag view

--- WalkieTalkie/WalkieTalkie/UI/Social/Social.ProfileViewcontrollerExtension.swift
+++ WalkieTalkie/WalkieTalkie/UI/Social/Social.ProfileViewcontrollerExtension.swift
@@ -824,7 +824,6 @@ extension Social.ProfileViewController {
             onlineStatusView.snp.makeConstraints { (maker) in
                 maker.trailing.equalToSuperview().offset(2.5)
                 maker.top.equalTo(-10)
-                maker.width.equalTo(64)
             }
             
         }
