21f4819e3244b6a6ae2b23c472788fac09d825a7
mayue <yue.ma@castbox.fm>
2021-06-22 11:07:18 +0800

    mod: add select friend UI

---
+++ WalkieTalkie/WalkieTalkie/UI/AmongUs/Home/Feed/Share/Feed.Share.SelectFriendsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  Feed.Share.SelectFriendsViewController.swift
+//  WalkieTalkie
+//
+//  Created by mayue on 2021/6/21.
+//  Copyright © 2021 Guru Rain. All rights reserved.
+//
+
+import UIKit
+
+extension Feed.Share {
+    
+    class SelectFriendsViewController: WalkieTalkie.ViewController {
+        
+    }
+    
+}
