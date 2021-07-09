61b57b4dc41db8243c500e10a0c3954d01bd5b95
mayue <yue.ma@castbox.fm>
2021-07-09 15:27:26 +0800

    mod: fix, remove tiktok from profile view

--- WalkieTalkie/WalkieTalkie/UI/Social/Profile/Social.ProfileFeedsViewController.swift
+++ WalkieTalkie/WalkieTalkie/UI/Social/Profile/Social.ProfileFeedsViewController.swift
@@ -15,15 +15,7 @@ extension Social {
     
     class ProfileFeedsViewController: WalkieTalkie.ViewController {
         
-        enum Option {
-            case tiktok
-            case feed
-        }
-        
         private var listViewDidScrollCallback: ((UIScrollView) -> ())?
-        
-        private typealias SectionHeader = Social.ProfileViewController.SectionHeader
-        private typealias ProfileTableCell = Social.ProfileViewController.ProfileTableCell
         
         private lazy var table: UICollectionView = {
             let layout = UICollectionViewFlowLayout()
@@ -37,10 +29,8 @@ extension Social {
             layout.minimumInteritemSpacing = 8
             let v = UICollectionView(frame: .zero, collectionViewLayout: layout)
             v.contentInset = UIEdgeInsets(top: 8, left: hInset, bottom: 0, right: hInset)
-            v.register(cellWithClazz: ProfileTableCell.self)
             v.register(cellWithClazz: FeedCell.self)
             v.register(cellWithClazz: CreateFeedCell.self)
-            v.register(supplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withClass: SectionHeader.self)
             v.showsVerticalScrollIndicator = false
             v.showsHorizontalScrollIndicator = false
             v.dataSource = self
@@ -60,14 +50,6 @@ extension Social {
             v.titleLabel.text = R.string.localizable.profileFeedNoVideos()
             v.isHidden = true
             return v
-        }()
-        
-        private lazy var options: [Option] = {
-//            if uid.isSelfUid {
-//                return [.feed]
-//            } else {
-                return [.feed]
-//            }
         }()
         
         private lazy var feeds = [Entity.Feed]() {
@@ -168,122 +150,44 @@ extension Social.ProfileFeedsViewController {
 // MARK: - UICollectionViewDataSource
 extension Social.ProfileFeedsViewController: UICollectionViewDataSource {
     
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return options.count
-    }
-    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
-        guard let op = options.safe(section) else {
-            return 0
-        }
-        
-        switch op {
-        
-        case .tiktok:
-            return 1
-            
-        case .feed:
-            
-            if uid.isSelfUid {
-                return max(1, feeds.count)
-            } else {
-                return feeds.count
-            }
-            
+        if uid.isSelfUid {
+            return max(1, feeds.count)
+        } else {
+            return feeds.count
         }
         
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        let op = options[indexPath.section]
-        
-        switch op {
-        
-        case .tiktok:
-            let cell = collectionView.dequeueReusableCell(withClazz: ProfileTableCell.self, for: indexPath)
-            cell.leftIconIV.image = R.image.ac_social_tiktok()
-            cell.titleLabel.text = R.string.localizable.profileShareTiktokTitle()
+        if let feed = feeds.safe(indexPath.item) {
+            let cell = collectionView.dequeueReusableCell(withClazz: FeedCell.self, for: indexPath)
+            cell.configCell(with: feed)
             return cell
-            
-        case .feed:
-            if let feed = feeds.safe(indexPath.item) {
-                let cell = collectionView.dequeueReusableCell(withClazz: FeedCell.self, for: indexPath)
-                cell.configCell(with: feed)
-                return cell
-            } else {
-                let cell = collectionView.dequeueReusableCell(withClazz: CreateFeedCell.self, for: indexPath)
-                cell.createAction = { [weak self] in
-                    let vc = Feed.SelectVideoViewController()
-                    self?.navigationController?.pushViewController(vc)
-                    Logger.Action.log(.profile_feed_create_clk)
-                }
-                return cell
+        } else {
+            let cell = collectionView.dequeueReusableCell(withClazz: CreateFeedCell.self, for: indexPath)
+            cell.createAction = { [weak self] in
+                let vc = Feed.SelectVideoViewController()
+                self?.navigationController?.pushViewController(vc)
+                Logger.Action.log(.profile_feed_create_clk)
             }
-            
+            return cell
         }
-        
     }
 }
 
 extension Social.ProfileFeedsViewController: UICollectionViewDelegate {
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
-        if let op = options.safe(indexPath.section) {
-            switch op {
-            
-            case .tiktok:
-                Logger.Action.log(.profile_tiktok_amongchat_tag_clk)
-                guard let url = URL(string: "https://www.tiktok.com/tag/amongchat") else {
-                    return
-                }
-                UIApplication.shared.open(url, options: [:]) { _ in
-                    
-                }
-                
-            case .feed:
-                
-                guard let feed = feeds.safe(indexPath.item),
-                      feed.statusType == .live else {
-                    return
-                }
-                
-                let liveIdx = liveFeeds.firstIndex { $0.pid == feed.pid } ?? 0
-                let vc = Social.ProfileFeedController(with: uid, dataSource: liveFeeds, index: liveIdx)
-                UIApplication.topViewController()?.navigationController?.pushViewController(vc)
-            }
-        }
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        
-        switch kind {
-        case UICollectionView.elementKindSectionHeader:
-            
-            let op = options[indexPath.section]
-            
-            let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withClass: SectionHeader.self, for: indexPath)
-            
-            switch op {
-            
-            case .tiktok:
-                header.titleLabel.text = R.string.localizable.amongChatProfileMakeTiktokVideo()
-                header.actionButton.isHidden = true
-                
-                
-            case .feed:
-                ()
-                
-            }
-            
-            return header
-            
-        default:
-            return UICollectionReusableView()
+        guard let feed = feeds.safe(indexPath.item),
+              feed.statusType == .live else {
+            return
         }
         
+        let liveIdx = liveFeeds.firstIndex { $0.pid == feed.pid } ?? 0
+        let vc = Social.ProfileFeedController(with: uid, dataSource: liveFeeds, index: liveIdx)
+        UIApplication.topViewController()?.navigationController?.pushViewController(vc)
     }
     
 }
@@ -292,51 +196,22 @@ extension Social.ProfileFeedsViewController: UICollectionViewDelegateFlowLayout 
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        guard let op = options.safe(indexPath.section) else {
-            return .zero
-        }
-        
         let padding: CGFloat = collectionView.contentInset.left + collectionView.contentInset.right
         
-        switch op {
-        
-        case .tiktok:
-            return CGSize(width: Frame.Screen.width - padding, height: 68)
+        if let _ = feeds.safe(indexPath.item) {
+            let columns: Int = 3
+            let interitemSpacing: CGFloat = 8
+            let hwRatio: CGFloat = 142.0 / 107.0
             
-        case .feed:
-            if let _ = feeds.safe(indexPath.item) {
-                let columns: Int = 3
-                let interitemSpacing: CGFloat = 8
-                let hwRatio: CGFloat = 142.0 / 107.0
-                
-                let cellWidth = ((UIScreen.main.bounds.width - padding - interitemSpacing * CGFloat(columns - 1)) / CGFloat(columns)).rounded(.towardZero)
-                let cellHeight = ceil(cellWidth * hwRatio)
-                
-                return CGSize(width: cellWidth, height: cellHeight)
-            } else {
-                return CGSize(width: UIScreen.main.bounds.width - padding, height: 267)
-            }
+            let cellWidth = ((UIScreen.main.bounds.width - padding - interitemSpacing * CGFloat(columns - 1)) / CGFloat(columns)).rounded(.towardZero)
+            let cellHeight = ceil(cellWidth * hwRatio)
             
+            return CGSize(width: cellWidth, height: cellHeight)
+        } else {
+            return CGSize(width: UIScreen.main.bounds.width - padding, height: 267)
         }
         
     }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        let op = options[section]
-        
-        switch op {
-        case .feed:
-            return .zero
-            
-        case .tiktok:
-            return CGSize(width: Frame.Screen.width, height: 27)
-        }
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
-        return .zero
-    }
-    
 }
 
 extension Social.ProfileFeedsViewController: UIScrollViewDelegate {
@@ -352,11 +227,11 @@ extension Social.ProfileFeedsViewController: JXPagingViewListViewDelegate {
     func listView() -> UIView {
         return view
     }
-
+    
     func listViewDidScrollCallback(callback: @escaping (UIScrollView) -> ()) {
         listViewDidScrollCallback = callback
     }
-
+    
     func listScrollView() -> UIScrollView {
         return table
     }
